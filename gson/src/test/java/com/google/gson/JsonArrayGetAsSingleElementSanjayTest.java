a3fc85723e255f63128b784d649dfe7d3cdb80cb
Sanjay Sivapragasam <sanjaysivapragasam12@gmail.com>
2026-03-29 13:09:57 -0400

    created tests for getassingleelement() and remove(json element) methods

---
+++ gson/src/test/java/com/google/gson/JsonArrayGetAsSingleElementSanjayTest.java
@@ -0,0 +1,38 @@
+package com.google.gson;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class JsonArrayGetAsSingleElementSanjayTest {
+
+    @Test
+    public void getAsSingleElement_sizeZero_throws() {
+        JsonArray array = new JsonArray();
+        try {
+            array.getAsString();
+            fail("Expected IllegalStateException");
+        } catch (IllegalStateException ex) {
+            assertEquals("Array must have size 1, but has size 0", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void getAsSingleElement_sizeOne_returnsElement() {
+        JsonArray array = new JsonArray();
+        array.add(new JsonPrimitive("hello"));
+
+        assertEquals("hello", array.getAsString());
+    }
+
+    @Test
+    public void getAsSingleElement_sizeGreaterThanOne_throws() {
+        JsonArray array = new JsonArray();
+        array.add(new JsonPrimitive("a"));
+        array.add(new JsonPrimitive("b"));
+        try {
+            array.getAsString();
+            fail("Expected IllegalStateException");
+        } catch (IllegalStateException ex) {
+            assertEquals("Array must have size 1, but has size 2", ex.getMessage());
+        }
+    }
+}
